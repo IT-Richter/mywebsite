7c6ec37c683479e23a4049912d5d262655422e4e
Kai <Kairichterback@hotmail.de>
2022-09-16 15:07:02 +0200

    core build indexpage

---
+++ views/core/index.php
@@ -0,0 +1,25 @@
+<?php require __DIR__ . "/../layout/header.php";?>
+    <main>
+        <article>
+            <header>
+                <h1>Kai Richter - Fachinformatiker Anwendungsentwicklung</h1>
+                <br>
+            </header>
+            <p>Ich bin Kai, 35 Jahre alt und lebe in Berlin. Ich bin Bäckermeister und Fachinformatiker für Anwendungsentwicklung.</p>
+            <p>Wie passt das den zusammen?</p>
+            <p>
+                Nach der Schule begann ich eine IT-Ausbildung am OSZ IMT. Diese habe ich abgebrochen, um im elterlichen
+                Familienunternehmen eine Ausbildung zum Bäcker zu absolvieren. Nachdem ich Bäckermeister geworden war,
+                bekam ich eine Mehlallergie und konnte somit den Beruf nicht weiter ausüben. Daraufhin ließ ich
+                mich mithilfe der BGN zum Fachinformatiker umschulen und landete somit wieder wie am Anfang in der IT.
+            </p>
+            <p>
+                Nun möchte ich auf dieser Website ein paar Projekte zum Leben erwecken und diese zum Üben und lernen nutzen.
+                Des Weiteren befinden sich Informationen zu bereits abgeschlossenen Projekten auf der Seite <a href="?page=projects">Projekte</a>.
+            </p>
+            <br>
+            <img src="../layout/img/kai.jpg" alt="picture of kai richter" >
+        </article>
+
+    </main>
+<?php require __DIR__ . "/../layout/footer.php";?>
---
+++ views/layout/footer.php
@@ -0,0 +1,7 @@
+<footer>
+    <p>&copy 2022 Kai Richter</p>
+    <hr>
+    <a href="mailto:it@krichter.de">it@krichter.de</a>
+</footer>
+</body>
+</html>
