6e98c3d22662df3e63684db12e8264c974f05589
Kai Richter <kairichterback@hotmail.de>
2022-09-23 16:55:25 +0200

    added laufende Projekte Abschnitt

--- views/core/projects.php
+++ views/core/projects.php
@@ -1,16 +1,18 @@
 <?php require __DIR__ . "/../layout/header.php"; ?>
 <main>
     <article>
-        <div class="container px-4 py-5" id="featured-3">
+        <div class="container px-4" id="featured-3">
             <h2 class="pb-2 border-bottom">Abgeschlossene Projekte</h2>
             <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
                 <div class="feature col">
                     <h3 class="fs-2">Was ziehe ich an?</h3>
-                    <p class="lead mb-4">Android APP. Geplant und erstellt von mir während meines Praktikums bei <a class="lead mb-4"
-                                href="https://www.liebenow.it/"> Liebenow IT</a>. Realisiert mit React Native und Expo.</p>
+                    <p class="lead mb-4">Android APP. Geplant und erstellt von mir während meines Praktikums bei <a
+                                class="lead mb-4"
+                                href="https://www.liebenow.it/"> Liebenow IT</a>. Realisiert mit React Native und Expo.
+                    </p>
                     <a href="https://play.google.com/store/apps/details?id=it.liebenow.Waszieheichan&gl=DE"
                        class="icon-link d-inline-flex align-items-center">
-                        Link to Google Play Store
+                        Link zum Google Play Store
                     </a>
                 </div>
                 <div class="feature col">
@@ -22,14 +24,29 @@
                     </a>
                 </div>
                 <div class="feature col">
-                    <h3 class="fs-2">APP für die IHK Prüfer (Fachinformatiker)</h3>
-                    <p class="lead mb-4">Eingabe von Prüfungsnoten und anschließender Ausgabe ob der Prüfling bestanden, nicht
+                    <h3 class="pb-5 fs-2">APP für die IHK Prüfer (Fachinformatiker)</h3>
+                    <p class="lead">Eingabe von Prüfungsnoten und anschließender Ausgabe ob der Prüfling bestanden,
+                        nicht
                         bestanden hat oder ob eine Nachprüfung möglich ist.</p>
                 </div>
             </div>
         </div>
     </article>
-
+    <article>
+        <div class="container px-4" id="featured-3">
+            <h2 class="pb-2 border-bottom">Laufende Projekte</h2>
+            <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
+                <div class="feature col">
+                    <h3 class="fs-2">Git Hub Repository</h3>
+                    <p class="lead mb-4">In meinem Git Hub Repository befinden sich meine aktuell öffentlichen laufenden
+                        Projekte</p>
+                    <a href="https://github.com/IT-Richter"
+                       class="icon-link d-inline-flex align-items-center">
+                        Link zum Git Hub Repository von Kai Richter
+                    </a>
+                </div>
+            </div>
+    </article>
 </main>
 
 <?php require __DIR__ . "/../layout/footer.php"; ?>
